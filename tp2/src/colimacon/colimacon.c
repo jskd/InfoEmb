317a6cce370b4c77c1d30371714ac94234c01e46
jskd <git@jeromeskoda.fr>
2017-10-16 23:28:14 +0200

    add doc

--- tp2/src/colimacon/colimacon.c
+++ tp2/src/colimacon/colimacon.c
@@ -3,27 +3,49 @@
 #include <string.h>
 #include "colimacon.h"
 
+/**
+ * @brief Borne de remplissage
+ */
 struct borne {
+  /// Borne haute
   int haute;
+  /// Borne droite
   int droite;
+  /// Borne basse
   int basse;
+  /// Borne gauche
   int gauche;
 };
 
+/**
+ * @brief Cursuer de postion de remplissage
+ */
 struct curseur {
+  /// Position dans les collones
   int col;
+  /// Position dans les lignes
   int ligne;
 };
 
+/**
+ * @brief Les differentes direction de remplissage
+ */
 typedef enum {
+  /// De bas vers le haut
   HAUT,
+  /// De droite vers la gauche
   GAUCHE,
+  /// De haut vers le bas
   BAS,
+  /// De gauche vers la droite
   DROITE
 } direction_t ;
 
 /**
- *  Print tableÒ
+ *  @brief Affiche un tableau en colimaçon
+ *  @param[in] table table à imprimmer
+ *  @param[in] line nombre de ligne de la table
+ *  @param[in] col nombre de colonnes de la table
  */
 void print_colimacon(int** table, int line, int col)
 {
@@ -35,6 +57,12 @@ void print_colimacon(int** table, int line, int col)
   }
 }
 
+/**
+ *  @brief Supression d'un tableau
+ *  @param[in] table table à supprimer
+ *  @param[in] line nombre de ligne de la table
+ *  @param[in] col nombre de colonnes de la table
+ */
 void delete_colimacon(int** table, int line, int col)
 {
   for(int i_line; i_line<line; i_line++)
@@ -42,6 +70,12 @@ void delete_colimacon(int** table, int line, int col)
   free(table);
 }
 
+/**
+ *  @brief Allocation d'un tableau
+ *  @param[in] line nombre de ligne de la table
+ *  @param[in] col nombre de colonnes de la table
+ *  @return pointeur vers le tableau
+ */
 static int** _malloc_colimacon(int line, int col) {
 
   int** table= malloc( line * sizeof(int *) );
@@ -51,6 +85,12 @@ static int** _malloc_colimacon(int line, int col) {
   return table;
 }
 
+/**
+ *  @brief Fonction d'iteration du curseur de remplissage en fonction
+ *         de la direction
+ *  @param[out] c curseur à déplacer
+ *  @param[in] d direction
+ */
 static void _iter_curseur( struct curseur* c, direction_t d ) {
   switch(d) {
     case HAUT:
@@ -68,6 +108,12 @@ static void _iter_curseur( struct curseur* c, direction_t d ) {
   }
 }
 
+/**
+ *  @brief Fonction d'iteration de borne de remplissage en fonction
+ *         de la direction
+ *  @param[out] b borne
+ *  @param[in] d direction
+ */
 static void _iter_borne( struct borne* borne, direction_t d) {
   switch(d) {
     case HAUT:
@@ -85,6 +131,12 @@ static void _iter_borne( struct borne* borne, direction_t d) {
   }
 }
 
+/**
+ * @brief Fonction de calcule de la positon initial pour remplir dans une direction
+ * @param[in] borne 
+ * @param[in] direction
+ * @return curseur
+ */
 static struct curseur _get_position_initiale(struct borne* borne, direction_t d) {
   struct curseur cur;
   switch(d) {
@@ -108,12 +160,24 @@ static struct curseur _get_position_initiale(struct borne* borne, direction_t d)
   return cur;
 }
 
+/**
+ * @brief Calcule si l'ecriture est possible en fonction des bornes
+ * @param[in] cur 
+ * @param[in] bor
+ * @return si true alors ecriture possible
+ */
 static char _canWrite(struct curseur* cur, struct borne* bor) {
   return cur->col > bor->gauche && cur->col < bor->droite
     && cur->ligne > bor->haute && cur->ligne < bor->basse;
 }
 
-
+/**
+ * @brief Ecriture complete vers une direction
+ * @param[in] tab tableau à remplir
+ * @param[in] bor borne du tableau 
+ * @param[in] val valeur à entrer
+ / @return si -1 aucune écriture sinon 0
+ */
 static int _ecriture_direction(int** tab, struct borne* bor, int* val,
   direction_t dir)
 {
@@ -134,7 +198,12 @@ static int _ecriture_direction(int** tab, struct borne* bor, int* val,
   return 0;
 }
 
-
+/**
+ * @brief Allocation d'un tableau et remplissage en colimacon
+ * @param[in] line nombre de ligne
+ * @param[in] col nombre de colonne
+ * @return retourne un pointer vers le tableau
+ */
 int** colimacon(int line, int col) {
 
   struct borne borne;
--- tp2/src/colimacon/colimacon.h
+++ tp2/src/colimacon/colimacon.h
@@ -1,10 +1,23 @@
-
 /**
- * @brief 
- *
+ * @brief Allocation d'un tableau et remplissage en colimacon
+ * @param[in] line nombre de ligne
+ * @param[in] col nombre de colonne
+ * @return retourne un pointer vers le tableau
  */
 int ** colimacon(int line, int col);
 
+/**
+ *  @brief Affiche un tableau en colimaçon
+ *  @param[in] table table à imprimmer
+ *  @param[in] line nombre de ligne de la table
+ *  @param[in] col nombre de colonnes de la table
+ */
 void print_colimacon(int** table, int line, int col);
 
+/**
+ *  @brief Supression d'un tableau
+ *  @param[in] table table à supprimer
+ *  @param[in] line nombre de ligne de la table
+ *  @param[in] col nombre de colonnes de la table
+ */
 void delete_colimacon(int** table, int line, int col);
